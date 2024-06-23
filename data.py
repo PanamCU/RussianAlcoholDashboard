a329021b72414adfbd91e8efbd0e610e372d058b
PanamCU <ed.farukshin@mail.ru>
2024-06-23 23:04:54 +0300

    region translation

--- data.py
+++ data.py
@@ -1,3 +1,3 @@
 import pandas as pd
 
-df = pd.read_csv('https://raw.githubusercontent.com/TripleSixGod0110/dataset/main/alchohol.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/PanamCU/alcohol_data/main/alcohol_data.csv')
